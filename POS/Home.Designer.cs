3673ac986f41dfd2a648fe03f4080a1fcf9e1f7e
humaiunkabir <humaiunkabir1983@gmail.com>
2019-02-09 12:13:25 +0600

    Update.

--- POS/Home.Designer.cs
+++ POS/Home.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.Logout = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblPosSystem = new System.Windows.Forms.Label();
             this.btnReport = new System.Windows.Forms.Button();
             this.btnInventory = new System.Windows.Forms.Button();
             this.btnSales = new System.Windows.Forms.Button();
             this.btnProduct = new System.Windows.Forms.Button();
+            this.lblPosSystem = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,26 +59,14 @@
             this.panel1.Location = new System.Drawing.Point(-336, -73);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1597, 1060);
+            this.panel1.Size = new System.Drawing.Size(1508, 1060);
             this.panel1.TabIndex = 3;
-            // 
-            // lblPosSystem
-            // 
-            this.lblPosSystem.AutoSize = true;
-            this.lblPosSystem.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
-                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPosSystem.Location = new System.Drawing.Point(323, -111);
-            this.lblPosSystem.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblPosSystem.Name = "lblPosSystem";
-            this.lblPosSystem.Size = new System.Drawing.Size(127, 29);
-            this.lblPosSystem.TabIndex = 4;
-            this.lblPosSystem.Text = "Pos System";
             // 
             // btnReport
             // 
             this.btnReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReport.Image = global::POS.Properties.Resources.Report;
-            this.btnReport.Location = new System.Drawing.Point(696, 549);
+            this.btnReport.Location = new System.Drawing.Point(708, 424);
             this.btnReport.Margin = new System.Windows.Forms.Padding(2);
             this.btnReport.Name = "btnReport";
             this.btnReport.Size = new System.Drawing.Size(208, 215);
@@ -92,7 +80,7 @@
             // 
             this.btnInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInventory.Image = global::POS.Properties.Resources.Inventory;
-            this.btnInventory.Location = new System.Drawing.Point(908, 549);
+            this.btnInventory.Location = new System.Drawing.Point(920, 424);
             this.btnInventory.Margin = new System.Windows.Forms.Padding(2);
             this.btnInventory.Name = "btnInventory";
             this.btnInventory.Size = new System.Drawing.Size(208, 215);
@@ -106,7 +94,7 @@
             // 
             this.btnSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSales.Image = global::POS.Properties.Resources.Sales;
-            this.btnSales.Location = new System.Drawing.Point(696, 326);
+            this.btnSales.Location = new System.Drawing.Point(708, 201);
             this.btnSales.Margin = new System.Windows.Forms.Padding(2);
             this.btnSales.Name = "btnSales";
             this.btnSales.Size = new System.Drawing.Size(208, 215);
@@ -120,7 +108,7 @@
             // 
             this.btnProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProduct.Image = global::POS.Properties.Resources.Product;
-            this.btnProduct.Location = new System.Drawing.Point(908, 326);
+            this.btnProduct.Location = new System.Drawing.Point(920, 201);
             this.btnProduct.Margin = new System.Windows.Forms.Padding(2);
             this.btnProduct.Name = "btnProduct";
             this.btnProduct.Size = new System.Drawing.Size(208, 215);
@@ -130,11 +118,23 @@
             this.btnProduct.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnProduct.UseVisualStyleBackColor = true;
             // 
+            // lblPosSystem
+            // 
+            this.lblPosSystem.AutoSize = true;
+            this.lblPosSystem.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosSystem.Location = new System.Drawing.Point(323, -111);
+            this.lblPosSystem.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPosSystem.Name = "lblPosSystem";
+            this.lblPosSystem.Size = new System.Drawing.Size(127, 29);
+            this.lblPosSystem.TabIndex = 4;
+            this.lblPosSystem.Text = "Pos System";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1262, 984);
+            this.ClientSize = new System.Drawing.Size(1262, 749);
             this.Controls.Add(this.Logout);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblPosSystem);
