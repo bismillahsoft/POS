fd68dbba42830416ad53a88fd6947794e2419dee
humaiunkabir <humaiunkabir1983@gmail.com>
2019-04-06 12:29:33 +0600

    Delete Option Completed.

--- POS/GenericEntry.cs
+++ POS/GenericEntry.cs
@@ -22,52 +22,49 @@ namespace POS
             _IProductGeneric = new BLL.BProductGeneric();
             Reset();
         }
-
+        int GenericID;
         private void btnGene_Save_Click(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 BO.ProductGeneric ObjProductGeneric = new BO.ProductGeneric();
 
                 ObjProductGeneric.GenericName = txtGenericName.Text;
                 ObjProductGeneric.GenericDescription = txtGenericDescription.Text;
 
-                if(btnGene_Save.Text=="Update" && ObjProductGeneric.GenericName != "")
+                if (btnGene_Save.Text == "Update" && ObjProductGeneric.GenericName != "")
                 {
-                    ObjProductGeneric.PGenericID = Convert.ToInt32(lblMessageBox.Text);
+                    ObjProductGeneric.PGenericID = GenericID;
                     if (_IProductGeneric.Update(ObjProductGeneric) > 0)
                     {
                         MessageBox.Show("Successfully Update");
                         Reset();
                     }
                 }
-                  else if (ObjProductGeneric.GenericName!="")
-                  {
+                else if (ObjProductGeneric.GenericName != "")
+                {
                     if (_IProductGeneric.Insert(ObjProductGeneric) > 0)
                     {
                         MessageBox.Show("Operation Success");
-                        //lblMessageBox.Text = "Operation Success";
-                        //lblMessageBox.ForeColor = Color.Green;
-                        // GetBrand();
                         Reset();
                     }
                     else
                     {
                         MessageBox.Show("Operation Failed");
-                        //lblMessageBox.Text = "Operation Failed";
-                        //lblMessageBox.ForeColor = System.Drawing.Color.Red;
-                        // lblMessageBox.Font.Bold = true;
                     }
-                   }
-                  else
+                }
+                else
                 {
                     MsgBox msgBox = new MsgBox();
                     msgBox.Show();
                 }
-                 }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
+            }
+            catch (Exception ex)
+            {
+                lblMessageBox.Text = ex.ToString();
+                lblMessageBox.Enabled = true;
+                lblMessageBox.ForeColor = Color.Red;
+            }
         }
         private void Reset()
         {
@@ -78,9 +75,16 @@ namespace POS
         }
         private void btnGene_Reset_Click(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 Reset();
-            } catch(Exception ex) { throw ex; }
+            }
+            catch (Exception ex)
+            {
+                lblMessageBox.Text = ex.ToString();
+                lblMessageBox.Enabled = true;
+                lblMessageBox.ForeColor = Color.Red;
+            }
         }
         private void GetGenericName()
         {
@@ -88,13 +92,22 @@ namespace POS
 
             productGenerics = _IProductGeneric.GetProductGenericList().ToList();
             grvGeneric.AutoGenerateColumns = false;
-            grvGeneric.DataSource = productGenerics; 
+            grvGeneric.DataSource = productGenerics;
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-            new Homeproduct().Show();
-            this.Hide();
+            try
+            {
+                new Homeproduct().Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                lblMessageBox.Text = ex.ToString();
+                lblMessageBox.Enabled = true;
+                lblMessageBox.ForeColor = Color.Red;
+            }
         }
         private void txtGenericName_KeyUp(object sender, KeyEventArgs e)
         {
@@ -122,18 +135,35 @@ namespace POS
         }
         private void grvGeneric_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if(e.ColumnIndex== grvGeneric.Columns["Edit"].Index && e.RowIndex >= 0)
+            try
             {
-                int numberRow = Convert.ToInt32(e.RowIndex);
-                var GenericID =grvGeneric.Rows[numberRow].Cells[0].Value.ToString();
-                lblMessageBox.Text = GenericID;
-                txtGenericName.Text = Convert.ToString(grvGeneric.Rows[numberRow].Cells[2].Value);
-                txtGenericDescription.Text = Convert.ToString(grvGeneric.Rows[numberRow].Cells[3].Value);
-                btnGene_Save.Text = "Update";
+                if (e.ColumnIndex == grvGeneric.Columns["Edit"].Index && e.RowIndex >= 0)
+                {
+                    int numberRow = Convert.ToInt32(e.RowIndex);
+                    GenericID = Convert.ToInt32(grvGeneric.Rows[numberRow].Cells[0].Value.ToString());
+                    //lblMessageBox.Text = GenericID.ToString();
+                    txtGenericName.Text = Convert.ToString(grvGeneric.Rows[numberRow].Cells[2].Value);
+                    txtGenericDescription.Text = Convert.ToString(grvGeneric.Rows[numberRow].Cells[3].Value);
+                    btnGene_Save.Text = "Update";
+                }
+                if (e.ColumnIndex == grvGeneric.Columns["Delete"].Index && e.RowIndex >= 0)
+                {
+                    int numberRow = Convert.ToInt32(e.RowIndex);
+                    GenericID = Convert.ToInt32(grvGeneric.Rows[numberRow].Cells[0].Value.ToString());
+
+                    if (_IProductGeneric.Delete(GenericID) > 0)
+                    {
+                        MessageBox.Show("Operation Success");
+                        Reset();
+                    }
+
+                }
             }
-            if(e.ColumnIndex== grvGeneric.Columns["Delete"].Index && e.RowIndex >= 0)
+            catch (Exception ex)
             {
-                lblMessageBox.Text = "Delete";
+                lblMessageBox.Text = ex.ToString();
+                lblMessageBox.Enabled = true;
+                lblMessageBox.ForeColor = Color.Red;
             }
         }
     }
