f1aafbc735eec3e5fa60eb52a7c82bb1d02a969a
shakilahammed20 <shakilahammed20@gmail.com>
2019-05-15 16:07:36 +0600

    Stock Working undone

--- POS/Program.cs
+++ POS/Program.cs
@@ -16,7 +16,7 @@ namespace POS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new StockEntry());
             //Application.Run(new Login());
         }
     }
--- POS/StockEntryPage.Designer.cs
+++ POS/StockEntryPage.Designer.cs
@@ -53,15 +53,20 @@
             this.lblProductName = new System.Windows.Forms.Label();
             this.lblStockEntry = new System.Windows.Forms.Label();
             this.txtPackSizee = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TransactionDate
             // 
+            this.TransactionDate.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TransactionDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.TransactionDate.Location = new System.Drawing.Point(344, 363);
-            this.TransactionDate.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.TransactionDate.Location = new System.Drawing.Point(900, 196);
+            this.TransactionDate.MinDate = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
             this.TransactionDate.Name = "TransactionDate";
-            this.TransactionDate.Size = new System.Drawing.Size(188, 20);
+            this.TransactionDate.Size = new System.Drawing.Size(108, 29);
             this.TransactionDate.TabIndex = 36;
             this.TransactionDate.Value = new System.DateTime(2019, 5, 5, 17, 50, 19, 0);
             this.TransactionDate.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TransactionDate_KeyUp);
@@ -70,7 +75,7 @@
             // 
             this.btnReset.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnReset.Location = new System.Drawing.Point(726, 454);
+            this.btnReset.Location = new System.Drawing.Point(726, 506);
             this.btnReset.Margin = new System.Windows.Forms.Padding(2);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(90, 35);
@@ -84,7 +89,7 @@
             // 
             this.btnSave.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnSave.Location = new System.Drawing.Point(837, 454);
+            this.btnSave.Location = new System.Drawing.Point(837, 506);
             this.btnSave.Margin = new System.Windows.Forms.Padding(2);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(90, 35);
@@ -110,7 +115,7 @@
             // 
             this.txtTsQty.Enabled = false;
             this.txtTsQty.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTsQty.Location = new System.Drawing.Point(820, 305);
+            this.txtTsQty.Location = new System.Drawing.Point(820, 357);
             this.txtTsQty.Name = "txtTsQty";
             this.txtTsQty.Size = new System.Drawing.Size(188, 29);
             this.txtTsQty.TabIndex = 45;
@@ -119,7 +124,7 @@
             // 
             this.lbl.AutoSize = true;
             this.lbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl.Location = new System.Drawing.Point(395, 190);
+            this.lbl.Location = new System.Drawing.Point(395, 235);
             this.lbl.Name = "lbl";
             this.lbl.Size = new System.Drawing.Size(65, 23);
             this.lbl.TabIndex = 26;
@@ -129,7 +134,7 @@
             // 
             this.txtTsPcs.Enabled = false;
             this.txtTsPcs.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTsPcs.Location = new System.Drawing.Point(593, 305);
+            this.txtTsPcs.Location = new System.Drawing.Point(593, 357);
             this.txtTsPcs.Name = "txtTsPcs";
             this.txtTsPcs.Size = new System.Drawing.Size(188, 29);
             this.txtTsPcs.TabIndex = 44;
@@ -138,7 +143,7 @@
             // 
             this.txtTsCtnPkt.Enabled = false;
             this.txtTsCtnPkt.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTsCtnPkt.Location = new System.Drawing.Point(344, 305);
+            this.txtTsCtnPkt.Location = new System.Drawing.Point(344, 357);
             this.txtTsCtnPkt.Name = "txtTsCtnPkt";
             this.txtTsCtnPkt.Size = new System.Drawing.Size(188, 29);
             this.txtTsCtnPkt.TabIndex = 43;
@@ -147,7 +152,7 @@
             // 
             this.txtCsCtn.Enabled = false;
             this.txtCsCtn.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCsCtn.Location = new System.Drawing.Point(344, 216);
+            this.txtCsCtn.Location = new System.Drawing.Point(344, 268);
             this.txtCsCtn.Name = "txtCsCtn";
             this.txtCsCtn.Size = new System.Drawing.Size(188, 29);
             this.txtCsCtn.TabIndex = 40;
@@ -156,7 +161,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(200, 308);
+            this.label1.Location = new System.Drawing.Point(241, 360);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 23);
             this.label1.TabIndex = 30;
@@ -166,7 +171,7 @@
             // 
             this.txtQty.Enabled = false;
             this.txtQty.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtQty.Location = new System.Drawing.Point(820, 261);
+            this.txtQty.Location = new System.Drawing.Point(820, 313);
             this.txtQty.Name = "txtQty";
             this.txtQty.Size = new System.Drawing.Size(188, 29);
             this.txtQty.TabIndex = 35;
@@ -175,7 +180,7 @@
             // txtPcs
             // 
             this.txtPcs.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPcs.Location = new System.Drawing.Point(593, 261);
+            this.txtPcs.Location = new System.Drawing.Point(593, 313);
             this.txtPcs.Name = "txtPcs";
             this.txtPcs.Size = new System.Drawing.Size(188, 29);
             this.txtPcs.TabIndex = 34;
@@ -185,7 +190,7 @@
             // txtCtnPkt
             // 
             this.txtCtnPkt.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCtnPkt.Location = new System.Drawing.Point(344, 261);
+            this.txtCtnPkt.Location = new System.Drawing.Point(344, 313);
             this.txtCtnPkt.Name = "txtCtnPkt";
             this.txtCtnPkt.Size = new System.Drawing.Size(188, 29);
             this.txtCtnPkt.TabIndex = 33;
@@ -207,7 +212,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(200, 264);
+            this.label11.Location = new System.Drawing.Point(241, 316);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(83, 23);
             this.label11.TabIndex = 28;
@@ -217,7 +222,7 @@
             // 
             this.lblCStock.AutoSize = true;
             this.lblCStock.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCStock.Location = new System.Drawing.Point(200, 219);
+            this.lblCStock.Location = new System.Drawing.Point(241, 271);
             this.lblCStock.Name = "lblCStock";
             this.lblCStock.Size = new System.Drawing.Size(103, 23);
             this.lblCStock.TabIndex = 27;
@@ -227,7 +232,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(668, 190);
+            this.label3.Location = new System.Drawing.Point(668, 235);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 23);
             this.label3.TabIndex = 25;
@@ -237,7 +242,7 @@
             // 
             this.txtCsPcs.Enabled = false;
             this.txtCsPcs.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCsPcs.Location = new System.Drawing.Point(593, 216);
+            this.txtCsPcs.Location = new System.Drawing.Point(593, 268);
             this.txtCsPcs.Name = "txtCsPcs";
             this.txtCsPcs.Size = new System.Drawing.Size(188, 29);
             this.txtCsPcs.TabIndex = 41;
@@ -246,7 +251,7 @@
             // 
             this.txtCsQty.Enabled = false;
             this.txtCsQty.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCsQty.Location = new System.Drawing.Point(820, 216);
+            this.txtCsQty.Location = new System.Drawing.Point(820, 268);
             this.txtCsQty.Name = "txtCsQty";
             this.txtCsQty.Size = new System.Drawing.Size(188, 29);
             this.txtCsQty.TabIndex = 42;
@@ -255,7 +260,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(887, 190);
+            this.label9.Location = new System.Drawing.Point(887, 235);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(36, 23);
             this.label9.TabIndex = 24;
@@ -265,7 +270,7 @@
             // 
             this.lblPackSIze.AutoSize = true;
             this.lblPackSIze.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPackSIze.Location = new System.Drawing.Point(576, 135);
+            this.lblPackSIze.Location = new System.Drawing.Point(589, 135);
             this.lblPackSIze.Name = "lblPackSIze";
             this.lblPackSIze.Size = new System.Drawing.Size(71, 23);
             this.lblPackSIze.TabIndex = 23;
@@ -275,7 +280,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(200, 361);
+            this.label10.Location = new System.Drawing.Point(751, 196);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(131, 23);
             this.label10.TabIndex = 22;
@@ -285,7 +290,7 @@
             // 
             this.lblProductName.AutoSize = true;
             this.lblProductName.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProductName.Location = new System.Drawing.Point(197, 135);
+            this.lblProductName.Location = new System.Drawing.Point(238, 139);
             this.lblProductName.Name = "lblProductName";
             this.lblProductName.Size = new System.Drawing.Size(104, 23);
             this.lblProductName.TabIndex = 31;
@@ -304,16 +309,56 @@
             // txtPackSizee
             // 
             this.txtPackSizee.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPackSizee.Location = new System.Drawing.Point(672, 135);
+            this.txtPackSizee.Location = new System.Drawing.Point(685, 135);
             this.txtPackSizee.Name = "txtPackSizee";
-            this.txtPackSizee.Size = new System.Drawing.Size(231, 26);
+            this.txtPackSizee.Size = new System.Drawing.Size(188, 26);
             this.txtPackSizee.TabIndex = 46;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(344, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 26);
+            this.textBox1.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(241, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 23);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Strip";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(593, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 26);
+            this.textBox2.TabIndex = 50;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(482, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 23);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Pcs Per Strip";
             // 
             // StockEntryPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 528);
+            this.ClientSize = new System.Drawing.Size(1182, 597);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPackSizee);
             this.Controls.Add(this.TransactionDate);
             this.Controls.Add(this.btnReset);
@@ -374,5 +419,9 @@
         private System.Windows.Forms.Label lblProductName;
         private System.Windows.Forms.Label lblStockEntry;
         private System.Windows.Forms.TextBox txtPackSizee;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
